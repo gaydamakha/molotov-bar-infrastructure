859dc970fc772d28afd3aa336c8cf88345e5eab2
gmikhail <mikhail.gaydamakha@gmail.com>
2023-07-08 20:07:51 +0200

    :sparkles: setup vpc for dev

---
+++ live/dev/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  org    = "Molotov Bar"
+  region = "eu-central-1"
+  env    = "dev"
+  account = {
+    master = "439575621641"
+  }
+  registry = {
+    molotov-bar-api = "molotov-bar-api"
+  }
+}
---
+++ live/dev/providers.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 5.7.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "molotov-bar"
+
+    workspaces {
+      name = "molotov-bar-infrastructure-dev"
+    }
+  }
+}
+
+provider "aws" {
+  region = local.region
+
+  assume_role {
+    role_arn = "arn:aws:iam::${local.account.master}:role/TerraformCloudDeployRole"
+  }
+
+  default_tags {
+    tags = {
+      Organization = local.org
+      Environment  = local.env
+      ManagedBy    = "Terraform"
+    }
+  }
+}
