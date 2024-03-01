76915e603bccdbb9721c88cec7d2c33ca07de293
gmikhail <mikhail.gaydamakha@gmail.com>
2024-03-01 06:32:37 +0100

    :fire: disable alb deletion protection

--- live/dev/alb.tf
+++ live/dev/alb.tf
@@ -6,6 +6,7 @@ module "alb" {
   name    = "${local.name}-alb"
   vpc_id  = module.vpc.vpc_id
   subnets = module.vpc.public_subnets
+  enable_deletion_protection = false
 
   # Security Group
   security_group_ingress_rules = {
